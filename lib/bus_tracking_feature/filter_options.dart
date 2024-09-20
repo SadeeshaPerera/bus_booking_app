f6cc636662844db41e215b854fb706640e5a4092
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-09-20 07:35:34 +0530

    refactor: Implement scrollable filter options

--- lib/bus_tracking_feature/filter_options.dart
+++ lib/bus_tracking_feature/filter_options.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
 
 class FilterOptions extends StatelessWidget {
+  const FilterOptions({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        FilterChipWidget(
-          icon: Icons.departure_board,
-          label: 'Departure time',
-        ),
-        FilterChipWidget(
-          icon: Icons.access_time,
-          label: 'Arrival',
-        ),
-        FilterChipWidget(
-          icon: Icons.price_change_outlined,
-          label: 'Price',
-        ),
-        // Add more filters here as needed
-      ],
+    return const SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          FilterChipWidget(
+            icon: Icons.departure_board_outlined,
+            label: 'Departure time',
+          ),
+          SizedBox(width: 8.0), // Add spacing between the chips
+          FilterChipWidget(
+            icon: Icons.access_time,
+            label: 'Arrival',
+          ),
+          SizedBox(width: 8.0), // Add spacing between the chips
+          FilterChipWidget(
+            icon: Icons.price_change_outlined,
+            label: 'Price',
+          ),
+          SizedBox(width: 8.0), // Add spacing between the chips
+          FilterChipWidget(
+            icon: Icons.timer_outlined,
+            label: 'Duration',
+          ),
+          SizedBox(width: 8.0), // Add spacing between the chips
+          FilterChipWidget(
+            icon: Icons.directions_bus_filled_outlined,
+            label: 'Arrival Station',
+          ),
+        ],
+      ),
     );
   }
 }
@@ -28,7 +43,7 @@ class FilterChipWidget extends StatelessWidget {
   final IconData icon;
   final String label;
 
-  FilterChipWidget({required this.icon, required this.label});
+  const FilterChipWidget({super.key, required this.icon, required this.label});
 
   @override
   Widget build(BuildContext context) {
