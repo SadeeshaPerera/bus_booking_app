c54971d5a09eaa7f0186733f10c998eb02ef23de
Devumi Hasangi <152767199+DHP0730@users.noreply.github.com>
2024-10-06 19:19:34 +0530

    feat : Created contact page

--- lib/Contact.dart
+++ lib/Contact.dart
@@ -7,85 +7,204 @@ class ContactScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contact Us'),
+        backgroundColor: Colors.blue[700],
+        title: const Text('Contact'),
+        centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context); // Navigate back
+            Navigator.pop(context);
           },
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/round_dp.png'), // Replace with your image asset
+            ),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'We are here to help you!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Contact Details:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            // Phone Number
-            ListTile(
-              leading: const Icon(Icons.phone),
-              title: const Text('+1 234 567 890'),
-              onTap: () {
-                // Call action
-                _makePhoneCall('+1234567890');
-              },
-            ),
-            const Divider(),
-            // Email
-            ListTile(
-              leading: const Icon(Icons.email),
-              title: const Text('support@busservice.com'),
-              onTap: () {
-                // Email action
-                _sendEmail('support@busservice.com');
-              },
-            ),
-            const Divider(),
-            // Address
-            ListTile(
-              leading: const Icon(Icons.location_on),
-              title: const Text('123 Bus Station, City, Country'),
-            ),
-            const Divider(),
-            // Website
-            ListTile(
-              leading: const Icon(Icons.web),
-              title: const Text('www.busservice.com'),
-              onTap: () {
-                // Open website action
-                _launchURL('https://www.busservice.com');
-              },
-            ),
-          ],
+      body: SingleChildScrollView( // Allow scrolling if content is too long
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, // Make content stretch to fill width
+            children: [
+              _buildCombinedContactContainer(),
+              const SizedBox(height: 16),
+              _buildBusCompanyContactContainer(),
+            ],
+          ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.confirmation_num_outlined),
+            label: 'Tickets',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.route),
+            label: 'Routes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.announcement_outlined),
+            label: 'Help',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
       ),
     );
   }
 
-  // Function to make a phone call
-  void _makePhoneCall(String phoneNumber) async {
-    // Implement your phone call functionality here
-    // For example, using url_launcher package
+  Widget _buildCombinedContactContainer() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 6,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Kandy Bus Station
+          Text(
+            'Kandy Bus Station',
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Phone: 0114563787'),
+                    SizedBox(height: 4),
+                    Text('Operating Hrs: 6:00am - 8:00pm'),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  'CALL NOW',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          const Divider(
+            thickness: 1.5,
+            color: Colors.grey,
+          ),
+          const SizedBox(height: 16),
+          // Jaffna Bus Station
+          Text(
+            'Jaffna Bus Station',
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Phone: 0114563787'),
+                    SizedBox(height: 4),
+                    Text('Operating Hrs: 6:00am - 8:00pm'),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  'CALL NOW',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
-  // Function to send an email
-  void _sendEmail(String email) async {
-    // Implement your email functionality here
-    // For example, using url_launcher package
-  }
-
-  // Function to launch a URL
-  void _launchURL(String url) async {
-    // Implement your URL launch functionality here
-    // For example, using url_launcher package
+  Widget _buildBusCompanyContactContainer() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 6,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Bus Company:',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const Text('Phone: 0114563787'),
+          const Text('Email: busguide@gmail.com'),
+          const Text('Operating Hours: 6:00am - 8:00pm'),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  'CALL',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
